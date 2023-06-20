2917b683f0f32a81d199fbd7e8d10c5018f35fc8
kainosmaxmuyunda <max.muyunda@kainos.com>
2023-06-20 20:16:28 +0100

    Add Sandbox Account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "aws_lz_acc@kainos.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws_lz_acc@kainos.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
